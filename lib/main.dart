20950cc6e489310a91cb536136986a6a67c66198
Zain Mehmood <106885156+zainwen9@users.noreply.github.com>
2024-02-16 19:14:09 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/screens/mobile_layout_screen.dart';
+import 'package:whatsapp/screens/web_layout_screen.dart';
+import 'package:whatsapp/utils/responsive_layout.dart';
+import 'colors.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
+  }
+}
